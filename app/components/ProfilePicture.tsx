c90cf7e016f2aa1ea7fe2c15ec420ed285fb1e0c
mayday <maydaythecoder@gmail.com>
2024-01-28 19:02:45 -0500

    fixed styling issues in the tasklist component

--- app/components/ProfilePicture.tsx
+++ app/components/ProfilePicture.tsx
@@ -3,69 +3,71 @@ import { authInstance, db, storageInstance } from "./firebase";
 import { collection, doc, getDoc } from "firebase/firestore";
 import { getDownloadURL, ref } from "firebase/storage";
 
-
 export default function ProfilePicture() {
-    const [profilePic, setprofilePic] = useState<string | undefined>("");
+  const [profilePic, setprofilePic] = useState<string | undefined>("");
 
+  useEffect(() => {
+    // Check who's logged in
+    const currentUser = authInstance.currentUser;
 
+    if (currentUser) {
+      const fetchFromStorage = async () => {
+        const allowedExtensions = [".png", ".jpg", ".jpeg", ".gif"];
+        let downloadURL;
 
-    useEffect(() => {
-        // Check who's logged in
-        const currentUser = authInstance.currentUser;
-      
-        if (currentUser) {
-            const fetchFromStorage = async () => {
-                const allowedExtensions = [".png", ".jpg", ".jpeg", ".gif"];
-                let downloadURL;
-        
-                for (const extension of allowedExtensions) {
-                  const storageRef = ref(
-                    storageInstance,
-                    `cover-photos/${currentUser.uid}/profilePic${extension}`
-                  );
-                  try {
-                    downloadURL = await getDownloadURL(storageRef);
-                    break; // Exit the loop if a valid downloadURL is found
-                  } catch (error: any) {
-                    if (error.code !== "storage/object-not-found") {
-                      // Handle other errors
-                      console.error("Error fetching download URL:", error);
-                    }
-                  }
-                }
-        
-                if (downloadURL) {
-                  setprofilePic(downloadURL);
-                } else {
-                  console.log("Profile picture not found in Storage with any of the allowed extensions.");
-                  // Handle the case where the file is not found
-                }
-              };
-              
-      
-          const fetchFromFirestore = async () => {
-            const docRef = doc(collection(db, "users"), currentUser.uid);
-            const docSnap = await getDoc(docRef);
-            if (docSnap.exists()) {
-              const userData = docSnap.data();
-              setprofilePic(userData?.photoURL);
+        for (const extension of allowedExtensions) {
+          // refrencing the location of the data
+          const storageRef = ref(
+            storageInstance,
+            `cover-photos/${currentUser.uid}/profilePic${extension}`
+          );
+          try {
+            // chech for a valid downloadURL
+            downloadURL = await getDownloadURL(storageRef);
+            break; // Exit the loop if a valid downloadURL is found
+          } catch (error: any) {
+            if (error.code !== "storage/object-not-found") {
+              // Handle other errors
+              console.error("Error fetching download URL:", error);
             }
-          };
-      
-          // Choose the preferred method
-          const method = "storage"; // or "firestore"
-      
-          if (method === "storage") {
-            fetchFromStorage();
-          } else {
-            fetchFromFirestore();
           }
         }
-      }, [authInstance.currentUser]); 
 
- console.log(profilePic)
-console.log(authInstance.currentUser?.photoURL)
- return (
+        if (downloadURL) {
+          setprofilePic(downloadURL);
+        } else {
+          console.log(
+            "Profile picture not found in Storage with any of the allowed extensions."
+          );
+          // Handle the case where the file is not found
+        }
+      };
+
+      const fetchFromFirestore = async () => {
+        // refrencing the location of the data
+        const docRef = doc(collection(db, "users"), currentUser.uid);
+        const docSnap = await getDoc(docRef);
+        // Fetch the data from the db
+        if (docSnap.exists()) {
+          const userData = docSnap.data();
+          setprofilePic(userData?.photoURL);
+        }
+      };
+
+      // Choose the preferred method
+      const method = "storage"; // or "firestore"
+
+      if (method === "storage") {
+        fetchFromStorage();
+      } else {
+        fetchFromFirestore();
+      }
+    }
+  }, [authInstance.currentUser]);
+
+  console.log(profilePic);
+  console.log(authInstance.currentUser?.photoURL);
+  return (
     <div className="absolute w-52 h-52">
       <img
         className="rounded-full bg-gray-50 overflow-hidden w-16 h-16"
